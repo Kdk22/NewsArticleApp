7f833dbb49f9598f492621c8373c4a8ad4ea9708
Utshab Khadka <khadkautshav7@gmail.com>
2021-08-17 13:19:17 -0400

    Cors Error Resolved

--- NewsArticle/Startup.cs
+++ NewsArticle/Startup.cs
@@ -20,7 +20,7 @@ namespace NewsArticle
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             //services.AddDbContext<ArticleContext>(options =>
             //                                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
@@ -30,6 +30,7 @@ namespace NewsArticle
                     Configuration.GetConnectionString("DataConnection"));
             });
 
+
             //services.AddSwaggerGen(c =>
             //{
             //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "NewsArticle", Version = "v1" });
@@ -38,9 +39,13 @@ namespace NewsArticle
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+
         {
             if (env.IsDevelopment())
             {
+                app.UseCors(
+                    options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                );
                 app.UseDeveloperExceptionPage();
                 //app.UseSwagger();
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NewsArticle v1"));
